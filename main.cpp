42d25d03bc5a0628276e7c548c4b2d737c3b2427
TanawinRobotis <tanawin.twox@gmail.com>
2023-04-21 14:01:35 +0700

    v1

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main() {
+    // Load the image from file
+    Mat image = imread("xxxai-center.png", IMREAD_COLOR);
+
+    // Check if the image was loaded successfully
+    if (image.empty()) {
+        printf("Could not open or find the image\n");
+        return -1;
+    }
+
+    // Create a window to display the image
+    namedWindow("Display window", WINDOW_AUTOSIZE);
+
+    // Show the image in the window
+    imshow("Display window", image);
+
+    // Wait for a keystroke in the window
+    waitKey(0);
+
+    return 0;
+}
